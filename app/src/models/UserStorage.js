b3c02379c09e1953698755ded0df7b36e8021eb8
kds <kddddds@gmail.com>
2023-01-19 00:45:45 +0000

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,16 +1,21 @@
 "use strict";
 
-// const { thistle } = require("color-name");
+const fs = require("fs").promises;
 
 class UserStorage {
-    static #users = {
-        id: ["kddddds", "나개발", "김팀장"],
-        psword: ["123", "1234", "123456"],
-        names: ["ㅏㅇㄴ", "나나", "김"]
-    };
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); // => [id, psword, name]
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
+    }
 
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -22,22 +27,22 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); // => [id, psword, name]
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-        return userInfo;
+        return fs
+            .readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+            })
+            .catch(console.error)
+            // .catch((err) => console.error(err));
     }
 
+
+
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.names.push(userInfo.name);
         users.psword.push(userInfo.psword);
-        console.log(users)
     }
 }
 
